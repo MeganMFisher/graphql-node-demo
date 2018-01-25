959f5e71fbba54b1abc45c5e1b1fdb6af3ff33bd
meganmfisher <meganmichellefisher@gmail.com>
2018-01-25 09:10:17 -0700

    new schema

--- resolver/resolvers/playlistTrackResolver.js
+++ resolver/resolvers/playlistTrackResolver.js
@@ -5,14 +5,9 @@ var getPlaylistTracks = function(args) {
     return psql.manyOrNone(playlistTracksQuery, [],  (e) => e )
 }
 
-var getMusicPlayListTracks = function(args) {
-    const musicPlaylistQuery = `SELECT "PlaylistTrack"."TrackId" FROM "PlaylistTrack"  JOIN "Playlist"  ON "Playlist"."PlaylistId" = "PlaylistTrack"."PlaylistId" WHERE "Playlist"."Name" = 'Music';`
-    return psql.manyOrNone(musicPlaylistQuery, [], (e) => e )
-}
 
 module.exports = {
     playlistTracks: getPlaylistTracks,
-    musicPlayListTracks: getMusicPlayListTracks 
 }
 
 // SELECT * FROM "PlaylistTrack" WHERE "PlaylistId" = ( SELECT "PlaylistId" FROM "Playlist" WHERE "Name" = ;'Music' )
--- schema/schemas/playlistTrackSchema.js
+++ schema/schemas/playlistTrackSchema.js
@@ -1,16 +1,10 @@
 
 module.exports = {
     query: 'playlistTracks: [PlaylistTrack]',
-    musicPlaylistTracks: 'musicPlayListTracks: [MusicPlayListTrack]',
     type: `
     type PlaylistTrack {
         PlaylistId: Int
         TrackId: Int
     }
-    `,
-    musicPlayListTracksType: `
-    type MusicPlayListTrack {
-        TrackId: Int
-
-    }`
+    `
 }
